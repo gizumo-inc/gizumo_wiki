11282617985b9b19778fa2f0558cff5aad33d1cb
佐藤涼太 <>
2021-05-06 15:29:34 +0900

    カテゴリー更新機能の実装

--- src/js/pages/Categories/Edit.vue
+++ src/js/pages/Categories/Edit.vue
@@ -3,7 +3,12 @@
     <app-category-edit
       :disabled="loading ? true : false"
       :access="access"
+      :update-category-name="updateCategoryName"
+      :error-message="errorMessage"
+      :done-message="doneMessage"
       @clearMessage="clearMessage"
+      @udpateValue="updateValue"
+      @handleSubmit="updateCategory"
     />
   </div>
 </template>
@@ -22,11 +27,33 @@ export default {
     loading() {
       return this.$store.state.categories.loading;
     },
+    errorMessage() {
+      return this.$store.state.categories.errorMessage;
+    },
+    doneMessage() {
+      return this.$store.state.categories.doneMessage;
+    },
+    updateCategoryName() {
+      return this.$store.state.categories.updateCategoryName;
+    },
   },
   methods: {
     clearMessage() {
       this.$store.dispatch('categories/clearMessage');
     },
+    updateValue($event) {
+      // console.log($event);
+      this.$store.dispatch('categories/editedCategoryName', $event.target.value);
+    },
+    updateCategory() {
+      if (this.loading) return;
+      this.$store.dispatch('categories/updateCategory');
+    },
+  },
+  created() {
+    const { id } = this.$route.params;
+    this.$store.dispatch('categories/getCategoryDetail', id);
+    this.$store.dispatch('categories/clearMessage');
   },
 };
 </script>
